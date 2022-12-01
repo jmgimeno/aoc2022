b324a4732de07e6d11c646dc6a0ba77ef9f4f62d
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2022-12-01 17:15:18 +0100

    solved day1 with streams

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ scalaVersion := "3.2.1"
 
 libraryDependencies ++= Seq(
   "dev.zio" %% "zio" % "2.0.4",
+  "dev.zio" %% "zio-streams" % "2.0.4",
   "dev.zio" %% "zio-test" % "2.0.4" % Test,
   "dev.zio" %% "zio-test-sbt" % "2.0.4" % Test,
   "dev.zio" %% "zio-test-magnolia" % "2.0.4" % Test
--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,7 +1,12 @@
 import zio.*
+import zio.stream.*
+
 import scala.io.Source
+import java.io.FileReader
 
 object Day1 extends ZIOAppDefault:
+
+  // with files
 
   def readFile(name: String): List[String] =
     Source.fromFile("data/day1.txt").getLines.toList
@@ -18,10 +23,35 @@ object Day1 extends ZIOAppDefault:
   def part2(lines: List[String]): Int =
     chunk(lines).map(g => g.map(_.toInt).sum).sortBy(n => -n).take(3).sum
 
-  val run = for
-    lines <- ZIO.attempt(readFile("data/day1.txt"))
-    p1 = part1(lines)
-    _ <- Console.printLine(s"part1 = $p1")
-    p2 = part2(lines)
-    _ <- Console.printLine(s"part2 = $p2")
-  yield ()
+  // with streams
+
+  val sumsStream =
+    ZStream
+      .fromFileName("data/day1.txt")
+      .via(ZPipeline.utf8Decode)
+      .via(ZPipeline.splitLines)
+      .split(_.isEmpty)
+      .map(chunk => chunk.map(_.toInt))
+      .map(_.sum)
+
+  val part1Result =
+    sumsStream
+      .runFold(0)(_ max _)
+
+  val part2Result =
+    sumsStream
+      .runFold[List[Int]](List.empty) { (max3, s) =>
+        (s :: max3).sortBy(n => -n).take(3)
+      }
+      .map(_.sum)
+
+  val run =
+    part1Result.debug *> part2Result.debug
+
+  // val run = for
+  //   lines <- ZIO.attempt(readFile("data/day1.txt"))
+  //   p1 = part1(lines)
+  //   _ <- Console.printLine(s"part1 = $p1")
+  //   p2 = part2(lines)
+  //   _ <- Console.printLine(s"part2 = $p2")
+  // yield ()
