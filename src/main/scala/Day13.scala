edb9f6783a39390382ad9d3988f2ea3408605ce5
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2022-12-13 11:19:34 +0100

    solved day13 part1

---
+++ src/main/scala/Day13.scala
@@ -0,0 +1,80 @@
+import zio.*
+import zio.stream.*
+
+import scala.collection.mutable
+import scala.util.chaining.scalaUtilChainingOps
+
+object Day13 extends ZIOAppDefault:
+
+  enum IntTree:
+    case Leaf(n: Int)
+    case Branch(children: List[IntTree])
+
+    def compareTo(other: IntTree): Int = (this, other) match
+      case (Leaf(lv), Leaf(rv))           => lv - rv
+      case (l @ Leaf(lv), r @ Branch(rc)) => Branch(List(l)).compareTo(r)
+      case (l @ Branch(lc), r @ Leaf(rv)) => l.compareTo(Branch(List(r)))
+      case (Branch(lc), Branch(rc)) =>
+        val comparisons = lc.zip(rc).map(_ compareTo _)
+        if comparisons.forall(_ == 0) then lc.length - rc.length
+        else comparisons.find(_ != 0).get
+
+  extension (c: Char)
+    def isEnd = c == '$'
+    def isBranchStart = c == '['
+    def isBranchEnd = c == ']'
+    def isComma = c == ','
+    def isLeafStart = c.isDigit
+    def isLeafEnd = c.isBranchEnd || c.isComma
+
+  object Parser:
+
+    def parseBlock(block: Chunk[String]) =
+      block.map(s => parseIntTree(s + "$")._1)
+
+    def parseIntTree(input: String): (IntTree, String) =
+      parseBranch(input)
+
+    def parseBranch(input: String): (IntTree, String) =
+      assert(input(0).isBranchStart)
+      val children = mutable.ListBuffer.empty[IntTree]
+      var rest = input.drop(1)
+      while !rest(0).isBranchEnd do
+        if rest(0).isComma then rest = rest.drop(1)
+        else if rest(0).isLeafStart then
+          val (leaf, newRest) = parseLeaf(rest)
+          children += leaf
+          rest = newRest
+        else if rest(0).isBranchStart then
+          val (branch, newRest) = parseBranch(rest)
+          children += branch
+          rest = newRest
+      (IntTree.Branch(children.toList), rest.drop(1))
+
+    def parseLeaf(input: String) =
+      assert(input(0).isLeafStart)
+      val (number, rest) = input.span(_.isDigit)
+      (IntTree.Leaf(number.toInt), rest)
+
+  def compare(trees: Chunk[IntTree], index: Long) =
+    trees(0).compareTo(trees(1))
+
+  val inputStream =
+    ZStream
+      .fromFileName("data/input13.txt")
+      .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
+      .orDie
+
+  def part1(is: UStream[String]): Task[Long] =
+    is.split(_.isEmpty)
+      .map(Parser.parseBlock)
+      .zipWithIndex
+      .filter((chunk, _) => chunk(0).compareTo(chunk(1)) < 0)
+      .map((_, index) => index + 1)
+      .runSum
+
+  def part2(is: UStream[String]): Task[Int] =
+    ZIO.succeed(-1)
+
+  lazy val run =
+    part1(inputStream).debug("PART1") *> part2(inputStream).debug("PART2")
