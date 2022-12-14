908f371bbaebf1963444c8979f448617f66ac9fb
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2022-12-14 14:05:51 +0100

    wip on day14

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -1,6 +1,8 @@
 import zio.*
 import zio.stream.*
 
+import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.util.chaining.scalaUtilChainingOps
 
 object Day14 extends ZIOAppDefault:
@@ -9,17 +11,34 @@ object Day14 extends ZIOAppDefault:
     case Horizontal(left: Int, right: Int, at: Int)
     case Vertical(top: Int, bottom: Int, at: Int)
 
+    def yMax = this match
+      case Horizontal(left, right, at) => at
+      case Vertical(top, bottom, at)   => bottom
+
     def occupies(coord: Coord) = (this, coord) match
       case (Horizontal(l, r, a), Coord(x, y)) => a == y && l <= x && x <= r
-      case (Vertical(t, b, a), Coord(x, y))   => a == y && t <= y && y <= b
+      case (Vertical(t, b, a), Coord(x, y))   => a == x && t <= y && y <= b
 
   case class Rock(segments: List[Segment]):
+    val yMax = segments.map(_.yMax).max
+
     def occupies(coord: Coord) =
       segments.exists(_.occupies(coord))
 
+    def escapes(coord: Coord) =
+      coord.y > yMax
+
   case class Coord(x: Int, y: Int):
-    def step: List[Coord] =
+    private def steps =
       List(Coord(x, y + 1), Coord(x - 1, y + 1), Coord(x + 1, y + 1))
+
+    def atRestOrEscaped(advance: Coord => Boolean): Coord =
+      @tailrec def forward(coord: Coord): Coord =
+        println(s"coord: $coord")
+        coord.steps.find(advance) match
+          case Some(next) => forward(next)
+          case None       => coord
+      forward(this)
 
   object Parser:
     def parsePath(line: String): List[Segment] =
@@ -41,6 +60,27 @@ object Day14 extends ZIOAppDefault:
     def parseCoords(coords: String) = coords match
       case s"$x,$y" => Coord(x.toInt, y.toInt)
 
+  case class SimulationResult(unitsAtRest: Int)
+
+  case class Simulator(rock: Rock):
+    def run: SimulationResult =
+      val atRest = mutable.Set.empty[Coord]
+      @tailrec def loop: SimulationResult =
+        val start = Coord(500, 0)
+        assert(!atRest(start) && !rock.occupies(start), "start should be free")
+        val stop =
+          start.atRestOrEscaped(c =>
+            !atRest(c) && !rock.occupies(c) && !rock.escapes(c)
+          )
+        println(s"start $start stop $stop")
+        if atRest.size > 1 then SimulationResult(-1)
+        if rock.escapes(stop) then SimulationResult(atRest.size)
+        else
+          atRest += stop
+          println(s"atrest: $atRest")
+          loop
+      loop
+
   lazy val inputStream =
     ZStream
       .fromFileName("data/input14.txt")
@@ -52,7 +92,7 @@ object Day14 extends ZIOAppDefault:
         is.flatMap(line => ZStream.fromIterable(Parser.parsePath(line)))
           .runCollect
           .map(_.toList.pipe(Rock.apply))
-    yield rock.segments.size
+    yield Simulator(rock).run.unitsAtRest
 
   def part2(is: UStream[String]): Task[Int] =
     ZIO.succeed(-1)
