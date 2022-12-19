e99dec9d7875610f044a3b2ef8a49089d6310cd1
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2022-12-19 12:14:25 +0100

    wip on day19

--- src/main/scala/aoc2022/Day19.scala
+++ src/main/scala/aoc2022/Day19.scala
@@ -3,9 +3,91 @@ package aoc2022
 import zio.*
 import zio.stream.*
 
+import math.Ordered.orderingToOrdered
+import scala.collection.mutable
 import scala.util.chaining.scalaUtilChainingOps
 
 object Day19 extends ZIOAppDefault:
+
+  enum Resource:
+    case Ore, Clay, Obsidian, Geode
+
+  case class Robot(makes: Resource, costs: Map[Resource, Int]):
+    def cost(r: Resource) = costs.getOrElse(r, 0)
+
+  enum RobotSpec:
+    case Ore(ore: Int)
+    case Clay(ore: Int)
+    case Obsidian(ore: Int, clay: Int)
+    case Geode(ore: Int, obsidian: Int)
+
+    def makeRobot: Robot = this match
+      case Ore(ore) =>
+        Robot(Resource.Ore, Map(Resource.Ore -> ore))
+      case Clay(ore) =>
+        Robot(Resource.Clay, Map(Resource.Ore -> ore))
+      case Obsidian(ore, clay) =>
+        Robot(Resource.Obsidian, Map(Resource.Ore -> ore, Resource.Clay -> clay))
+      case Geode(ore, obsidian) =>
+        Robot(Resource.Geode, Map(Resource.Ore -> ore, Resource.Obsidian -> obsidian))
+
+  case class Blueprint(id: Int, specifications: Map[Resource, RobotSpec]):
+    def makeRobot(resource: Resource) = specifications(resource).makeRobot
+
+  object Blueprint:
+    import RobotSpec.*
+    def parse(line: String): Blueprint =
+      val patternNum = raw"Blueprint (\d+):".r
+      val patternOre = raw"Each ore robot costs (\d+) ore.".r
+      val patternClay = raw"Each clay robot costs (\d+) ore.".r
+      val patternObsidian = raw"Each obsidian robot costs (\d+) ore and (\d+) clay.".r
+      val patternGeode = raw"Each geode robot costs (\d+) ore and (\d+) obsidian.".r
+      val num = patternNum.findFirstMatchIn(line).get.group(1).toInt
+      val costOreRobot = patternOre.findFirstMatchIn(line).get.group(1).toInt
+      val costClayRobot = patternClay.findFirstMatchIn(line).get.group(1).toInt
+      val costObsidianRobotOre = patternObsidian.findFirstMatchIn(line).get.group(1).toInt
+      val costObsidianRobotClay = patternObsidian.findFirstMatchIn(line).get.group(2).toInt
+      val costGeodeRobotOre = patternGeode.findFirstMatchIn(line).get.group(1).toInt
+      val costGeodeRobotClay = patternGeode.findFirstMatchIn(line).get.group(2).toInt
+      Blueprint(
+        num,
+        Map(
+          Resource.Ore -> Ore(costOreRobot),
+          Resource.Clay -> Clay(costClayRobot),
+          Resource.Obsidian -> Obsidian(costObsidianRobotOre, costObsidianRobotClay),
+          Resource.Geode -> Geode(costGeodeRobotOre, costGeodeRobotClay)
+        )
+      ).tap(println)
+
+  case class State(
+      time: Int,
+      blueprint: Blueprint,
+      robots: Map[Resource, Int],
+      resources: Map[Resource, Int]
+  ):
+    def next: List[State] = ???
+
+  object State:
+    given Ordering[State] with
+      def compare(left: State, right: State) =
+        left.resources(Resource.Geode).compare(right.resources(Resource.Geode))
+
+  class Simulation(blueprint: Blueprint, maxTime: Int):
+    def qualityLevel: Int = blueprint.id * maxGeodes
+    def maxGeodes: Int =
+      val robots = Resource.values.map(r => (r, if r == Resource.Ore then 1 else 0)).toMap
+      val resources = Resource.values.map(_ -> 0).toMap
+      val initial = State(0, blueprint, robots, resources)
+      var best = initial
+      val fringe = mutable.PriorityQueue(initial)
+      while !fringe.isEmpty do
+        val current = fringe.dequeue()
+        if current.time == maxTime
+        then
+          if current > best then best = current
+        else fringe.enqueue(current.next*)
+
+      best.resources(Resource.Geode)
 
   lazy val inputStream =
     ZStream
@@ -14,7 +96,7 @@ object Day19 extends ZIOAppDefault:
       .orDie
 
   def part1(is: UStream[String]): Task[Int] =
-    ZIO.succeed(-1)
+    is.map(Blueprint.parse).map(Simulation(_, 24).qualityLevel).runSum.map(_.toInt)
 
   def part2(is: UStream[String]): Task[Int] =
     ZIO.succeed(-1)
--- src/test/scala/aoc2022/Day19Suite.scala
+++ src/test/scala/aoc2022/Day19Suite.scala
@@ -12,7 +12,8 @@ import Day19.*
 object Day19Suite extends ZIOSpecDefault:
 
   val example =
-    """""".stripMargin
+    """Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+      |Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.""".stripMargin
 
   val exampleStream = ZStream.fromIterable(example.split("\n"))
 
@@ -21,7 +22,7 @@ object Day19Suite extends ZIOSpecDefault:
       suite("part1")(
         test("example") {
           assertZIO(part1(exampleStream))(equalTo(0))
-        } @@ ignore,
+        },
         test("input.txt") {
           assertZIO(part1(inputStream))(equalTo(0))
         } @@ ignore
