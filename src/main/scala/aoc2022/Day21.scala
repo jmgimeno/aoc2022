06eea3eefa54dd8466dd04b4e789eb6b0f4dcdc7
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2022-12-21 08:06:36 +0100

    solved day21 part1

--- src/main/scala/aoc2022/Day21.scala
+++ src/main/scala/aoc2022/Day21.scala
@@ -7,16 +7,64 @@ import scala.util.chaining.scalaUtilChainingOps
 
 object Day21 extends ZIOAppDefault:
 
+  enum Job:
+    case Num(n: Long)
+    case Sum(left: String, right: String)
+    case Dif(left: String, right: String)
+    case Mul(left: String, right: String)
+    case Div(left: String, right: String)
+
+  object Job:
+    def parse(line: String): (String, Job) = line match
+      case s"$monkey: $left + $right" => (monkey, Sum(left, right))
+      case s"$monkey: $left - $right" => (monkey, Dif(left, right))
+      case s"$monkey: $left * $right" => (monkey, Mul(left, right))
+      case s"$monkey: $left / $right" => (monkey, Div(left, right))
+      case s"$monkey: $num"           => (monkey, Num(num.toLong))
+
+  class Evaluator(jobs: Map[String, Job]):
+    def yell(monkey: String): Long =
+      val environment = jobs.collect { case (monkey, Job.Num(num)) => (monkey, num) }.toMap
+      val stack = List(monkey)
+      def loop(stack: List[String], environment: Map[String, Long]): Long =
+        if environment.contains(monkey) then environment(monkey)
+        else
+          stack match
+            case head :: next =>
+              if environment.contains(head) then loop(next, environment)
+              else
+                jobs(head) match
+                  case Job.Num(num) => loop(next, environment.updated(head, num))
+                  case Job.Sum(left, right) =>
+                    if environment.contains(left) && environment.contains(right) then
+                      loop(next, environment.updated(head, environment(left) + environment(right)))
+                    else loop(left :: right :: stack, environment)
+                  case Job.Dif(left, right) =>
+                    if environment.contains(left) && environment.contains(right) then
+                      loop(next, environment.updated(head, environment(left) - environment(right)))
+                    else loop(left :: right :: stack, environment)
+                  case Job.Mul(left, right) =>
+                    if environment.contains(left) && environment.contains(right) then
+                      loop(next, environment.updated(head, environment(left) * environment(right)))
+                    else loop(left :: right :: stack, environment)
+                  case Job.Div(left, right) =>
+                    if environment.contains(left) && environment.contains(right) then
+                      loop(next, environment.updated(head, environment(left) / environment(right)))
+                    else loop(left :: right :: stack, environment)
+            case Nil => assert(false, "should never happen")
+      loop(stack, environment)
+
   lazy val inputStream =
     ZStream
       .fromFileName("data/input21.txt")
       .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
       .orDie
 
-  def part1(is: UStream[String]): Task[Int] =
-    ZIO.succeed(-1)
+  def part1(is: UStream[String]): Task[Long] =
+    for parsedLines <- is.map(Job.parse).runCollect.map(_.toMap)
+    yield Evaluator(parsedLines).yell("root")
 
-  def part2(is: UStream[String]): Task[Int] =
+  def part2(is: UStream[String]): Task[Long] =
     ZIO.succeed(-1)
 
   lazy val run =
--- src/test/scala/aoc2022/Day21Suite.scala
+++ src/test/scala/aoc2022/Day21Suite.scala
@@ -12,7 +12,21 @@ import Day21.*
 object Day21Suite extends ZIOSpecDefault:
 
   lazy val example =
-    """""".stripMargin
+    """root: pppw + sjmn
+      |dbpl: 5
+      |cczh: sllz + lgvd
+      |zczc: 2
+      |ptdq: humn - dvpt
+      |dvpt: 3
+      |lfqf: 4
+      |humn: 5
+      |ljgn: 2
+      |sjmn: drzm * dbpl
+      |sllz: 4
+      |pppw: cczh / lfqf
+      |lgvd: ljgn * ptdq
+      |drzm: hmdt - zczc
+      |hmdt: 32""".stripMargin
 
   lazy val exampleStream = ZStream.fromIterable(example.split("\n"))
 
@@ -20,11 +34,11 @@ object Day21Suite extends ZIOSpecDefault:
     suite("day21")(
       suite("part1")(
         test("example") {
-          assertZIO(part1(exampleStream))(equalTo(0))
-        } @@ ignore,
+          assertZIO(part1(exampleStream))(equalTo(152L))
+        },
         test("input.txt") {
-          assertZIO(part1(inputStream))(equalTo(0))
-        } @@ ignore
+          assertZIO(part1(inputStream))(equalTo(158731561459602L))
+        }
       ),
       suite("part2")(
         test("example") {
