30917bc099d8e5f370d25ba0a8fb0ddc201b0010
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2023-01-12 17:00:31 +0100

    changed to scala-cli

---
+++ build.scala
@@ -0,0 +1,5 @@
+//> using scala "3.2.1"
+//> using lib "dev.zio::zio:2.0.5"
+//> using lib "dev.zio::zio-streams:2.0.5"
+//> using lib "dev.zio::zio-test:2.0.5"
+//> using lib "dev.zio::zio-test-sbt:2.0.5"
