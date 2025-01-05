52362ab6b4e5a8ac5652958e304a0a91c710efe7
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2025-01-05 18:24:13 +0100

    New board representation and parsing

--- src/main/scala/aoc2022/Day22.scala
+++ src/main/scala/aoc2022/Day22.scala
@@ -10,115 +10,109 @@ import scala.util.Failure
 import scala.util.Success
 import scala.annotation.tailrec
 
-object Day22 extends ZIOAppDefault:
+object Day22 extends ZIOAppDefault {
 
-  extension [A](matrix: Array[Array[A]])
-    def apply(position: Position) = Try {
-      matrix(position.y)(position.x)
-    }
-    def columnsAt(position: Position) =
-      matrix(position.y).length
-    def rowsAt =
-      matrix.length
+  enum Tile(symbol: Char) {
+    case Open extends Tile('.')
+    case Wall extends Tile('#')
 
-  enum Tile:
-    case Null, Open, Wall
+    override def toString: String = symbol.toString
+  }
 
-  case class Notes private (tiles: Array[Array[Tile]]):
-    def ahead(status: Status, n: Int): Position =
-      val Status(position @ Position(x, y), orientation) = status
-      @tailrec def loop(current: Position, lastOpen: Position, steps: Int): Position =
-        if steps == n then lastOpen
-        else
-          val nextPosition = current + orientation
-          tiles(nextPosition) match
-            case Failure(exception) =>
-              orientation match
-                case Orientation.Right =>
-                  loop(nextPosition.copy(x = -1), lastOpen, steps)
-                case Orientation.Down =>
-                  loop(nextPosition.copy(y = -1), lastOpen, steps)
-                case Orientation.Left =>
-                  val lastColumn = tiles.columnsAt(nextPosition)
-                  loop(nextPosition.copy(x = lastColumn), lastOpen, steps)
-                case Orientation.Up =>
-                  val lastRow = tiles.rowsAt
-                  loop(nextPosition.copy(y = lastRow), lastOpen, steps)
-            case Success(Tile.Null) => loop(nextPosition, lastOpen, steps)
-            case Success(Tile.Open) => loop(nextPosition, nextPosition, steps + 1)
-            case Success(Tile.Wall) => lastOpen
-      loop(position, position, 0)
-
-  object Notes:
-    def make(tiles: Array[Array[Tile]]) =
-      val maxLength = tiles.map(_.length).max
-      val normalized = tiles.map(row => row ++ Array.fill(maxLength - row.length)(Tile.Null))
-      Notes(normalized)
-
-  enum Step:
+  enum Step {
     case Ahead(n: Int)
     case Clockwise
     case CounterClockwise
+  }
 
   case class Path(steps: List[Step])
 
-  case class Position(x: Int, y: Int):
+  case class Position(x: Int, y: Int) {
     infix def +(orientation: Orientation): Position =
       Position(x + orientation.dx, y + orientation.dy)
     infix def -(other: Position): Position =
       Position(x - other.x, y - other.y)
+  }
 
-  enum Orientation(val dx: Int, val dy: Int):
+  enum Orientation(val dx: Int, val dy: Int) {
     case Right extends Orientation(1, 0)
     case Down extends Orientation(0, 1)
     case Left extends Orientation(-1, 0)
     case Up extends Orientation(0, -1)
-    def counterClockWise = this match
-      case Right => Up
-      case Down  => Right
-      case Left  => Down
-      case Up    => Left
-    def clockWise = this match
-      case Right => Down
-      case Down  => Left
-      case Left  => Up
-      case Up    => Right
+  }
 
-  case class Status(position: Position, orientation: Orientation):
-    def finalPassword =
-      val row = position.y + 1
-      val col = position.x + 1
-      val orient = orientation.ordinal
-      1000 * row + 4 * col + orient
+  import Orientation.*
 
-  class Walker(notes: Notes):
-    val start = Status(Position(notes.tiles(0).indexWhere(_ != Tile.Null), 0), Orientation.Right)
-    def walk(path: Path): Int =
-      path.steps
-        .foldLeft(start) { case (status @ Status(position, orientation), step) =>
-          step match
-            case Step.Ahead(n)         => Status(notes.ahead(status, n), orientation)
-            case Step.Clockwise        => Status(position, orientation.clockWise)
-            case Step.CounterClockwise => Status(position, orientation.counterClockWise)
+  class Face(id: Int, bigX: Int, bigY: Int, tiles: Array[Array[Tile]]) {
+
+    val size = tiles.length
+
+    def apply(y: Int)(x: Int): Tile = tiles(y)(x)
+
+    override def toString: String =
+      s"id: $id, bigX: $bigX, bigY: $bigY, size: $size\n" +
+        tiles.map(_.mkString).mkString("\n")
+  }
+
+  class Board(val faces: Array[Face], faceMap: Map[Int, Map[Orientation, Int]]) {
+    def apply(f: Int)(y: Int)(x: Int): Tile = faces(f)(y)(x)
+
+    override def toString: String =
+      faces.map(_.toString).mkString("\n")
+  }
+
+  object Parser {
+
+    def parseTile(c: Char) = {
+      c match
+        case '.' => Tile.Open
+        case '#' => Tile.Wall
+    }
+
+    def parseStep(step: String): Step = {
+      step match
+        case "R" => Step.Clockwise
+        case "L" => Step.CounterClockwise
+        case n   => Step.Ahead(n.toInt)
+    }
+
+    def parseBoard(
+        block: Chunk[String],
+        shape: String,
+        size: Int,
+        faceMap: Map[Int, Map[Orientation, Int]]
+    ): Board = {
+      val tiles = Array.ofDim[Tile](6, size, size)
+
+      def position(shape: String, f: Int): (Int, Int) = {
+        val y = shape.split("\n").indexWhere(_.contains(f.toString))
+        val x = shape.split("\n")(y).indexOf(f.toString)
+        (x, y)
+      }
+
+      def parseFace(f: Int, bigX: Int, bigY: Int): Unit = {
+        val minY = bigY * size
+        val minX = bigX * size
+        for (y <- 0 until size) {
+          val line = block(minY + y)
+          for (x <- 0 until size) {
+            val c = line(minX + x)
+            tiles(f)(y)(x) = parseTile(c)
+          }
         }
-        .finalPassword
+      }
 
-  object Parser:
+      var faces = Array.ofDim[Face](6)
+      for (f <- 0 until 6) {
+        val (bigX, bigY) = position(shape, f)
+        parseFace(f, bigX, bigY)
+        faces(f) = new Face(f, bigX, bigY, tiles(f))
+      }
 
-    def parseTile(c: Char) = c match
-      case ' ' => Tile.Null
-      case '.' => Tile.Open
-      case '#' => Tile.Wall
+      Board(faces, faceMap)
+    }
 
-    def parseStep(step: String): Step = step match
-      case "R" => Step.Clockwise
-      case "L" => Step.CounterClockwise
-      case n   => Step.Ahead(n.toInt)
-
-    def parseNotes(block: Chunk[String]): Notes =
-      Notes.make(Array.from(block.map(line => Array.from(line.map(parseTile)))))
-
-    def parsePath(block: Chunk[String]): Path =
+    def parsePath(block: Chunk[String]): Path = {
       val st = new StringTokenizer(block(0), "RL", true)
       Path {
         Iterator
@@ -128,22 +122,53 @@ object Day22 extends ZIOAppDefault:
           .map(parseStep)
           .toList
       }
+    }
+  }
 
-  lazy val inputStream =
+  lazy val inputStream = {
     ZStream
       .fromFileName("data/input22.txt")
       .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
       .orDie
+  }
 
-  def part1(is: UStream[String]): Task[Int] =
-    for
+  def part1(
+      is: UStream[String],
+      shape: String,
+      size: Int,
+      faceMap: Map[Int, Map[Orientation, Int]]
+  ): Task[Int] = {
+    for {
       blocks <- is.split(_.isEmpty).runCollect
-      notes = Parser.parseNotes(blocks(0))
+      board = Parser.parseBoard(blocks(0), shape, size, faceMap)
       path = Parser.parsePath(blocks(1))
-    yield Walker(notes).walk(path)
+    } yield 0
+  }
 
-  def part2(is: UStream[String]): Task[Int] =
+  def part2(is: UStream[String]): Task[Int] = {
     ZIO.succeed(-1)
+  }
 
-  lazy val run =
-    part1(inputStream).debug("PART1") *> part2(inputStream).debug("PART2")
+  val inputShape =
+    """ 01
+      | 2
+      |34
+      |5""".stripMargin
+
+  val inputSize = 50
+
+  val inputFaceMap: Map[Int, Map[Orientation, Int]] =
+    Map(
+      0 -> Map(Right -> 1, Down -> 2, Left -> 1, Up -> 4),
+      1 -> Map(Right -> 0, Down -> 1, Left -> 0, Up -> 1),
+      2 -> Map(Right -> 2, Down -> 4, Left -> 2, Up -> 0),
+      3 -> Map(Right -> 4, Down -> 5, Left -> 4, Up -> 5),
+      4 -> Map(Right -> 3, Down -> 0, Left -> 3, Up -> 2),
+      5 -> Map(Right -> 5, Down -> 3, Left -> 5, Up -> 3)
+    )
+
+  lazy val run = {
+    part1(inputStream, inputShape, inputSize, inputFaceMap).debug("PART1") *> part2(inputStream)
+      .debug("PART2")
+  }
+}
--- src/test/scala/aoc2022/Day22Suite.scala
+++ src/test/scala/aoc2022/Day22Suite.scala
@@ -8,6 +8,7 @@ import zio.test.Assertion.*
 import zio.test.TestAspect.*
 
 import Day22.*
+import Day22.Orientation.*
 
 object Day22Suite extends ZIOSpecDefault:
 
@@ -27,17 +28,36 @@ object Day22Suite extends ZIOSpecDefault:
       |
       |10R5L5R10L4R5L5""".stripMargin
 
+  val exampleShape =
+    """  0
+      |123
+      |  45""".stripMargin
+
+  val exampleFaceMap: Map[Int, Map[Orientation, Int]] =
+    Map(
+      0 -> Map(Right -> 0, Down -> 3, Left -> 0, Up -> 4),
+      1 -> Map(Right -> 2, Down -> 1, Left -> 2, Up -> 1),
+      2 -> Map(Right -> 3, Down -> 2, Left -> 1, Up -> 2),
+      3 -> Map(Right -> 1, Down -> 4, Left -> 2, Up -> 0),
+      4 -> Map(Right -> 4, Down -> 0, Left -> 5, Up -> 3),
+      5 -> Map(Right -> 4, Down -> 5, Left -> 4, Up -> 5)
+    )
+
   val exampleStream = ZStream.fromIterable(example.split("\n"))
+
+  val exampleSize = 4
 
   val spec =
     suite("day22")(
       suite("part1")(
         test("example") {
-          assertZIO(part1(exampleStream))(equalTo(6032))
+          assertZIO(part1(exampleStream, exampleShape, exampleSize, exampleFaceMap))(equalTo(6032))
         },
         test("input.txt") {
-          assertZIO(part1(inputStream))(equalTo(149250))
-        }
+          assertZIO(part1(inputStream, Day22.inputShape, Day22.inputSize, Day22.inputFaceMap))(
+            equalTo(149250)
+          )
+        } @@ ignore
       ),
       suite("part2")(
         test("example") {
